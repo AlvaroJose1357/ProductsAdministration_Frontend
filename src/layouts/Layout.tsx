34a274c6698e030cf9524d3d5b50670bca3314e5
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-02-03 22:04:27 -0500

    add Header component and integrate it into Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function Layout() {
   return (
     <>
-      <div> desde Layout</div>;
-      <Outlet />
+      <Header />
+      <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
+        <Outlet />
+      </main>
     </>
   );
 }
