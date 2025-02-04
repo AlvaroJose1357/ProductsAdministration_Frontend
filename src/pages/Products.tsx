75475be1f3b467a88e82ba9a740877ca861549fc
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-02-03 22:32:06 -0500

    add ErrorMessage component and implement form validation in NewProduct page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-black text-slate-500">Productos</h1>
+        <Link
+          to={"/products/new"}
+          className="rounded-md bg-indigo-700 p-3 text-sm font-bold text-white shadow-md hover:bg-indigo-500">
+          Nuevo producto
+        </Link>
+      </div>
+    </>
+  );
 }
