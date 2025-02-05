cf238a914958224a637293c03693ec7c2b9ab96b
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-02-04 20:06:48 -0500

    add product validation schema and enhance addProduct function with error handling

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,22 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../schemas/product-schema";
+
 type ProductDataProps = {
   [k: string]: FormDataEntryValue;
 };
 
 export async function addProduct(data: ProductDataProps) {
-  console.log("Adding product", data);
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+    if (result.success) {
+    } else {
+      throw new Error("Invalid data");
+    }
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
